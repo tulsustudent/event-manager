e8d78ee03203b3f4651729f73a659d536934920b
tulsustudent <mdriamov@gmail.com>
2026-07-20 01:05:54 +0300

    feat: add registration endpoint and user service

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,13 +1,27 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import relationship
 from backend.app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    events = relationship("Event", back_populates="owner")
+
 
 class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String)
     description = Column(String)
-    category = Column(String, index=True)
+    category = Column(String)
     is_private = Column(Boolean, default=False)
     event_date = Column(DateTime)
-    creator_id = Column(Integer)  # Пока заглушка: просто ID создателя
+    creator_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="events")
--- backend/app/db/schemas.py
+++ backend/app/db/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import List, Optional
 
 class EventBase(BaseModel):
     title: str
@@ -15,6 +17,19 @@ class EventCreate(EventBase):
 class Event(EventBase):
     id: int
     creator_id: int
+
+    class Config:
+        from_attributes = True
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    events: List[Event] = []
 
     class Config:
         from_attributes = True
